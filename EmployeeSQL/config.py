e1d605f6b8a856810014f4aaed7d439d46adb17f
Connor Roemelt <connor.roemelt@gmail.com>
2020-02-13 16:01:26 -0600

    push assignment

---
+++ EmployeeSQL/config.py
@@ -0,0 +1,1 @@
+password = 'Scarywheel72#'
--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -2,6 +2,7 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Drop Tables if Existing
+-- I need this a couple of times so I'm just leaving it in
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
@@ -11,35 +12,36 @@ DROP TABLE IF EXISTS titles;
 
 
 -- SQL Homework Map
+-- Removed quotation marks that came with the file download from quickDB
 CREATE TABLE departments (
     dept_no VARCHAR   NOT NULL,
     dept_name VARCHAR   NOT NULL,
     CONSTRAINT pk_departments PRIMARY KEY (
-        dept_no
+        "dept_no"
      )
 );
 
 CREATE TABLE dept_emp (
     emp_no INT   NOT NULL,
     dept_no VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL
 );
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR   NOT NULL,
     emp_no INT   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no INT   NOT NULL,
-    birth_date DATE   NOT NULL,
+    birth_date VARCHAR   NOT NULL,
     first_name VARCHAR   NOT NULL,
     last_name VARCHAR   NOT NULL,
     gender VARCHAR   NOT NULL,
-    hire_date DATE   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
     CONSTRAINT pk_employees PRIMARY KEY (
         emp_no
      )
@@ -48,37 +50,20 @@ CREATE TABLE employees (
 CREATE TABLE salaries (
     emp_no INT   NOT NULL,
     salary INT   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL
 );
 
 CREATE TABLE titles (
     emp_no INT   NOT NULL,
     title VARCHAR   NOT NULL,
-    from_date DATE   NOT NULL,
-    to_date DATE   NOT NULL
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL
 );
 
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
+------------------------------
 
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
-REFERENCES departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-----------------------------------
-
+-- Read in CSVs
 COPY departments(dept_no,dept_name)
 FROM 'C:\data\departments.csv' DELIMITER ',' CSV HEADER;
 
@@ -96,3 +81,24 @@ FROM 'C:\data\salaries.csv' DELIMITER ',' CSV HEADER;
 
 COPY titles(emp_no,title,from_date,to_date)
 FROM 'C:\data\titles.csv' DELIMITER ',' CSV HEADER;
+
+------------------------------
+
+-- Adding Foreign Keys
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
